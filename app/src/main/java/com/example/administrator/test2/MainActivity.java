718bc93a08bff7aa4d3647e8429c1d86c5fbcea8
fukaitao <bryantfkt216@163.com>
2017-04-15 00:37:52 +0800

    模块 Test 添加修改+*) 增加fragment demo

--- app/src/main/java/com/example/administrator/test2/MainActivity.java
+++ app/src/main/java/com/example/administrator/test2/MainActivity.java
@@ -1,18 +1,57 @@
 package com.example.administrator.test2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RadioGroup;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment1 fragment1;
+    private Fragment2 fragment2;
+    private Fragment3 fragment3;
+    private FragmentManager fragmentManager;
+    private RadioGroup radioGroup;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //fukaitao add
-        //fukaitao add for test 2017年4月9日00:05:38
+        initView();
     }
 
     public static String toString() {
+    }
+}
+
+    private void initView() {
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragmentManager = getSupportFragmentManager();
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new onCheckedChangeListener());
+    }
+
+    //单选点击监听
+    private class onCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.radioButton1:
+                    fragmentManager.beginTransaction().replace(R.id.content, new Fragment1()).addToBackStack(null).commit();
+                    break;
+                case R.id.radioButton2:
+                    fragmentManager.beginTransaction().replace(R.id.content, new Fragment2()).addToBackStack(null).commit();
+                    break;
+                case R.id.radioButton3:
+                    fragmentManager.beginTransaction().replace(R.id.content, new Fragment3()).addToBackStack(null).commit();
+                    break;
+            }
+        }
     }
 }
